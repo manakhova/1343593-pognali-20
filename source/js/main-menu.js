e72f549b3b4d102e847a81fd7b3c4e26861fa4e3
Tatiana Manakhova <tat.manakhova@gmail.com>
2020-09-13 23:58:49 +0300

    Добавляет векторный спрайт

--- source/js/main-menu.js
+++ source/js/main-menu.js
@@ -2,8 +2,6 @@ var header = document.querySelector('.page-header');
 var headerButton = document.querySelector('.page-header__button');
 var headerNav = document.querySelector('.page-header__navigation');
 var headerCont = document.querySelector('.page-header__actions');
-var headerContIconPhone = document.querySelector('.contacts__icon--phone');
-var headerContIconMail = document.querySelector('.contacts__icon--mail');
 
 header.classList.remove('page-header--nojs');
 
@@ -14,8 +12,6 @@ headerButton.addEventListener('click', function() {
     headerButton.classList.add('page-header__button--opened');
     headerNav.classList.add('site-navigation--scroll');
     headerCont.classList.add('contacts--scroll');
-    headerContIconPhone.classList.add('contacts__icon--scroll--phone');
-    headerContIconMail.classList.add('contacts__icon--scroll--mail');
   } else {
     header.classList.remove('page-header--scroll');
     headerButton.classList.remove('page-header__button--opened');
